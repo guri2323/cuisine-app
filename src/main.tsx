fbdc1386d1f7e9a1c9163941169b464552e1de94
Gurleen Singh <learning2989@gmail.com>
2025-11-19 20:19:51 -0500

    dsds

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <BrowserRouter
+      basename="/cuisine-app/"
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
