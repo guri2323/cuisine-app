a994880c4a8a6b184d852574fa20025a651434b6
Gurleen Singh <learning2989@gmail.com>
2025-11-19 20:43:01 -0500

    added scroll to top of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import ScrollToTop from "./components/ScrollToTop";
 import HomePage from "./pages/HomePage";
 import CuisineDetailPage from "./pages/CuisineDetailPage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -7,6 +8,7 @@ import NotFoundPage from "./pages/NotFoundPage";
 function App() {
   return (
     <Layout>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cuisine/:id" element={<CuisineDetailPage />} />
